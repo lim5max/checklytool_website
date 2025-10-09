8c2af95406fde086dab09f4df855ad237cfdcd4b
Максим Штиль <lionmaks2017@yandex.ru>
2025-10-09 20:31:26 +0300

    fix: Удалить неиспользуемый импорт Card из profile-content

--- components/profile/profile-content.tsx
+++ components/profile/profile-content.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Card } from '../ui/card'
 import { Button } from '../ui/button'
 import { User, Zap, Check, TrendingUp, Calendar } from 'lucide-react'
 import Image from 'next/image'
